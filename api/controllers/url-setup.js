a4939051633bf580f7844c276db9a5f58e4545d0
Taj485 <tajbi21@gmail.com>
2019-12-18 15:25:10 +0000

    app is working, test fails to access functions

--- api/controllers/url-setup.js
+++ api/controllers/url-setup.js
@@ -1,41 +1,50 @@
-function getWebUrl(params){
-    var websites ={};
-        for (var url in params) {
-            if (params[url] != null) {
-                websites[url] = params[url];
-                console.log(websites[url]);
-                console.log(params[url]);
+var UrlSetup = function () {
+
+    getWebUrl = function (params){
+        var websites ={};
+            for (var url in params) {
+                if (params[url] != null) {
+                    websites[url] = params[url];
+                    console.log(websites[url]);
+                    console.log(params[url]);
+                }
             }
-        }
-        if (websites[Object.keys(websites)[0]] == null)
-        {
-           websites = setDefaultUrl();
-        }
-    return websites;
-}
-
-function setDefaultUrl(){
-    const websites = {
-        websites1: "www.hotel-internet-marketing.com/",
-        websites2: "www.bbc.co.uk/",
-        websites3: "www.google.co.uk/"
-    }
-    return websites;
-}
-
-function setUpQuery(websites) {
-    const api = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed';
+            if (websites[Object.keys(websites)[0]] == null)
+            {
+               websites = setDefaultUrl();
+            }
+        return websites;
+    };
     
-    const api_url = {};
-
-    for (key in websites) {
-
-        api_url[key] = api + `?url=https://${websites[key]}`
-        console.log(api_url[key]);
-    }
+    setUpQuery = function(websites) {
+        const api = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed';
+        
+        const api_url = {};
     
-    return api_url;
-}
+        for (key in websites) {
+    
+            api_url[key] = api + `?url=https://${websites[key]}`
+            console.log(api_url[key]);
+        }
+        
+        return api_url;
+    }
 
-module.exports.getWebUrl = getWebUrl;
-module.exports.setUpQuery = setUpQuery;
+    this.setDefaultUrl = function(){
+        const websites = {
+            websites1: "www.hotel-internet-marketing.com/",
+            websites2: "www.bbc.co.uk/",
+            websites3: "www.google.co.uk/"
+        }
+        return websites;
+    }
+
+    return {
+        getWebUrl: getWebUrl,
+        setUpQuery: setUpQuery
+    };
+
+}();
+ 
+exports.UrlSetup = UrlSetup;
+
--- api/routes/page-speed.js
+++ api/routes/page-speed.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const loadTimer = require('../services/page-speed-api');
-const pageSetup = require('../controllers/url-setup');
-
+const UrlSetup = require('../controllers/url-setup');
 
 router.get('/:website1?/:website2?/:website3?', async (req, res) => {
 
-    const websites =  pageSetup.getWebUrl(req.params);
+    const websites = UrlSetup.UrlSetup.getWebUrl(req.params);
 
-    const api_url =  pageSetup.setUpQuery(websites);
+    const api_url =  UrlSetup.UrlSetup.setUpQuery(websites);
 
     const pageSpeedInfo = await loadTimer.pageSpeedApiAsync(api_url);
     
